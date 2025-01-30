886136b58d97312d821467ccaad42669d0891020
yohaisim <yohaisimhony@gmail.com>
2025-01-30 11:32:43 +0200

    setting updated

--- server.py
+++ server.py
@@ -117,7 +117,7 @@ def update_click_count():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=5000)
 
 def handler(event, context):
     return app(event, context)
